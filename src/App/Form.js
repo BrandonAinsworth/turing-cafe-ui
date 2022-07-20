82a5e779e446eaa8d7325fae87750ef83ec9892e
Brandon Ainsworth <brandonaainsworth@gmail.com>
2022-07-20 10:06:56 -0600

    Add form properties and onChange function

--- src/App/Form.js
+++ src/App/Form.js
@@ -11,6 +11,48 @@ class Form extends Component {
             number: ''
         }
 }
+
+onChange = (event) => {
+    this.setState({
+        [event.target.name]: event.target.value
+    });
+}
+
+render() {
+    return (
+        <form>
+            <input 
+            type='text'
+            name="name"
+            placeholder="Name"
+            value={this.state.name}
+            onChange={event => this.onChange(event)}
+            />
+             <input 
+            type='text'
+            name="date"
+            placeholder="Date (mm/dd)"
+            value={this.state.date}
+            onChange={event => this.onChange(event)}
+            />
+             <input 
+            type='text'
+            name="time"
+            placeholder="Time"
+            value={this.state.time}
+            onChange={event => this.onChange(event)}
+            />
+             <input 
+            type='text'
+            name="number"
+            placeholder="Number of guests"
+            value={this.state.number}
+            onChange={event => this.onChange(event)}
+            />
+           <button>Make Reservation</button>
+        </form>
+    )
+}
 }
 
 export default Form
