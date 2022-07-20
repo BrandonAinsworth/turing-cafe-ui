ebda08b82be9e78e6b512f4cee5329ed64f3ce80
Brandon Ainsworth <brandonaainsworth@gmail.com>
2022-07-20 09:28:24 -0600

    Initial commit, add GET and create reservations.js

--- src/App/App.js
+++ src/App/App.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: []
+    }
+  }
+
+
+componentDidMount = () => {
+
+  fetch('http://localhost:3001/api/v1/reservations')
+    .then(res => res.json())
+    .then(reserves => {
+      this.setState({
+        reservations: reserves
+      })
+    })
+}
+
   render() {
     return (
       <div className="App">
+        {console.log(this.state.reservations)}
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
 
         </div>
         <div className='resy-container'>
-          
+          <Reservations reservations={this.state.reservations}/>
         </div>
       </div>
     )
